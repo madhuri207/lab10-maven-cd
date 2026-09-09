95c959540bf978844b641dcbbc1275ef6a9e3d9b
Madhuri Sangani <madhurisangani007@gmail.com>
2026-09-09 21:33:05 +0530

    Initial commit for Lab 10

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven!");
+        System.out.println("Java application built and packaged successfully.");
+    }
+}
